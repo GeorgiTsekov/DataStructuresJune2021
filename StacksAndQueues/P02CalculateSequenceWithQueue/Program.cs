b067de68a6759d04f17986f7b7aec34519823b02
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-06-15 12:18:34 +0300

    StacksAndQueues problems

---
+++ StacksAndQueues/P02CalculateSequenceWithQueue/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P02CalculateSequenceWithQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            Queue<int> queue = new Queue<int>();
+            queue.Enqueue(number);
+            Queue<int> newStack = new Queue<int>();
+            newStack.Enqueue(number);
+            for (int i = 0; i < 50; i++)
+            {
+                int newNumber = newStack.Dequeue();
+
+                newStack.Enqueue(newNumber + 1);
+                newStack.Enqueue(newNumber * 2 + 1);
+                newStack.Enqueue(newNumber + 2);
+                queue.Enqueue(newNumber + 1);
+                queue.Enqueue(newNumber * 2 + 1);
+                queue.Enqueue(newNumber + 2);
+            }
+
+            for (int i = 0; i < 50; i++)
+            {
+                if (i == 49)
+                {
+                    Console.WriteLine(queue.Dequeue());
+                }
+                else
+                {
+                    Console.Write(queue.Dequeue() + ", ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ StacksAndQueues/WithStack/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WithStack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToList();
+
+            Stack<int> stack = new Stack<int>();
+
+            foreach (var number in input)
+            {
+                stack.Push(number);
+            }
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                Console.Write(stack.Pop() + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
