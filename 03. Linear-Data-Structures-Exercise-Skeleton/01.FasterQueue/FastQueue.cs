204b6d6e913a5eff2c25ccdb09084a61cf6b8f14
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-07-07 17:35:35 +0300

    Some problems with fasterQueue

---
+++ 03. Linear-Data-Structures-Exercise-Skeleton/01.FasterQueue/FastQueue.cs
@@ -0,0 +1,62 @@
+﻿namespace Problem01.FasterQueue
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    public class FastQueue<T> : IAbstractQueue<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+        public int Count { get; private set; }
+
+        public bool Contains(T item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Dequeue()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Enqueue(T item)
+        {
+            var newNode = new Node<T>(item, null);
+            if (this.Count == 0)
+            {
+                //this.tail = newNode;
+                //this.head = this.tail;
+
+                this.head = this.tail = newNode;
+                this.Count++;
+                return;
+            }
+
+            this.tail.Next = newNode;
+            this.tail = this.tail.Next;
+            this.Count++;
+        }
+
+        public T Peek()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            var node = this.head;
+
+            while (node != null)
+            {
+                yield return node.Item;
+                node = node.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
