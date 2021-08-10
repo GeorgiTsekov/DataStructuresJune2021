f86a5c8aeddf3bcbdb523c054d595126f278531d
Georgi Tsekov <georgi_tsekov@abv.bg>
2021-08-10 22:20:56 +0300

    test problems

--- Tests/05.Trees-Representation-and-Traversal-(BFS-DFS)-Exercise-Skeleton (1)/Tree/Tree.cs
+++ Tests/05.Trees-Representation-and-Traversal-(BFS-DFS)-Exercise-Skeleton (1)/Tree/Tree.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
     using System.Text;
 
     public class Tree<T> : IAbstractTree<T>
@@ -57,32 +58,390 @@
 
         public Tree<T> GetDeepestLeftomostNode()
         {
-            throw new NotImplementedException();
+            var leafNodes = this.GetLeafNodes();
+            var list = new List<T>();
+            Tree<T> tree = leafNodes.FirstOrDefault();
+
+            foreach (var leaf in leafNodes)
+            {
+                var currentNode = new List<T>();
+                var node = leaf;
+
+                while (node != null)
+                {
+                    currentNode.Add(node.Key);
+                    node = node.Parent;
+                }
+
+                if (currentNode.Count > list.Count)
+                {
+                    currentNode.Reverse();
+                    list = currentNode;
+                    tree = leaf;
+                }
+            }
+
+            return tree;
+
+            //var queue = new Queue<Tree<T>>();
+
+            //queue.Enqueue(this);
+
+            //while (queue.Count != 0)
+            //{
+            //    var node = queue.Dequeue();
+
+            //    foreach (var child in node.Children)
+            //    {
+            //        queue.Enqueue(child);
+            //        if (child.Children.Count == 0)
+            //        {
+            //            return child;
+            //        }
+            //        break;
+            //    }
+            //}
+
+            //return null;
         }
 
         public List<T> GetLeafKeys()
         {
-            throw new NotImplementedException();
+            var leafNodes = this.GetLeafNodes();
+
+            return leafNodes.Select(x => x.Key).ToList();
+
+            //var result = new List<T>();
+            //var queue = new Queue<Tree<T>>();
+            //queue.Enqueue(this);
+
+            //while (queue.Count != 0)
+            //{
+            //    var node = queue.Dequeue();
+
+            //    foreach (var child in node.Children)
+            //    {
+            //        if (child.Children.Count == 0)
+            //        {
+            //            result.Add(child.Key);
+            //        }
+
+            //        queue.Enqueue(child);
+            //    }
+            //}
+
+            //return result;
+        }
+
+        private List<Tree<T>> GetLeafNodes()
+        {
+            var leafNodes = new List<Tree<T>>();
+
+            var queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            while (queue.Count != 0)
+            {
+                var node = queue.Dequeue();
+
+                if (node.Children.Count == 0)
+                {
+                    leafNodes.Add(node);
+                }
+
+                foreach (var child in node.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return leafNodes;
         }
 
         public List<T> GetMiddleKeys()
         {
-            throw new NotImplementedException();
+            var result = new List<T>();
+            var queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            while (queue.Count != 0)
+            {
+                var node = queue.Dequeue();
+
+                foreach (var child in node.Children)
+                {
+                    if (child.Children.Count > 0 && child.Parent != null)
+                    {
+                        result.Add(child.Key);
+                    }
+
+                    queue.Enqueue(child);
+                }
+            }
+
+            return result;
         }
 
         public List<T> GetLongestPath()
         {
-            throw new NotImplementedException();
+            var leafNodes = this.GetLeafNodes();
+            var result = new List<T>();
+
+            foreach (var leaf in leafNodes)
+            {
+                var currentNode = new List<T>();
+                var node = leaf;
+
+                while (node != null)
+                {
+                    currentNode.Add(node.Key);
+                    node = node.Parent;
+                }
+
+                if (currentNode.Count > result.Count)
+                {
+                    currentNode.Reverse();
+                    result = currentNode;
+                }
+            }
+
+            return result;
+
+
+            //var result = new List<T>();
+            //var queue = new Queue<Tree<T>>();
+            //queue.Enqueue(this);
+            //result.Add(this.Key);
+
+            //while (queue.Count != 0)
+            //{
+            //    var node = queue.Dequeue();
+            //    foreach (var child in node.Children)
+            //    {
+            //        queue.Enqueue(child);
+            //        result.Add(child.Key);
+            //        break;
+            //    }
+            //}
+            //return result;
         }
 
         public List<List<T>> PathsWithGivenSum(int sum)
         {
-            throw new NotImplementedException();
+            var result = new List<List<T>>();
+            var currentSum = 0;
+
+            this.PathsWithSumDFS(this, ref currentSum, sum, result, new List<T>());
+
+            return result;
+
+            //var leafNodes = this.GetLeafNodes();
+
+            //var result = new List<List<T>>();
+
+            //foreach (var leaf in leafNodes)
+            //{
+            //    var node = leaf;
+            //    var currentSum = 0;
+            //    var currentNode = new List<T>();
+
+            //    while (node != null)
+            //    {
+            //        currentNode.Add(node.Key);
+            //        currentSum += Convert.ToInt32(node.Key);
+            //        node = node.Parent;
+            //    }
+
+            //    if (currentSum == sum)
+            //    {
+            //        currentNode.Reverse();
+            //        result.Add(currentNode);
+            //    }
+            //}
+
+            //return result;
+
+            //var result = new List<List<T>>();
+
+            //var queue = new Queue<Tree<T>>();
+            //queue.Enqueue(this);
+
+            //while (queue.Count != 0)
+            //{
+            //    var currentSum = 0;
+            //    var node = queue.Dequeue();
+            //    var list = new List<T>();
+
+            //    foreach (var child in node.Children)
+            //    {
+            //        if (child.Children.Count == 0)
+            //        {
+            //            var childNode = child;
+
+            //            while (childNode.Parent != null)
+            //            {
+            //                list.Add(childNode.Key);
+            //                currentSum += Convert.ToInt32(childNode.Key);
+            //                childNode = childNode.Parent;
+            //            }
+
+            //            list.Add(childNode.Key);
+            //            currentSum += Convert.ToInt32(childNode.Key);
+
+            //            if (currentSum == sum)
+            //            {
+            //                list.Reverse();
+            //                result.Add(list);
+            //            }
+
+            //            currentSum = 0;
+            //            list = new List<T>();
+            //        }
+
+            //        queue.Enqueue(child);
+            //    }
+            //}
+
+            //return result;
+        }
+
+        private void PathsWithSumDFS(
+            Tree<T> node,
+            ref int currentSum,
+            int targetSum,
+            List<List<T>> allPaths,
+            List<T> currentPathValues)
+        {
+            currentPathValues.Add(node.Key);
+            currentSum += Convert.ToInt32(node.Key);
+
+            foreach (var child in node.Children)
+            {
+                this.PathsWithSumDFS(child, ref currentSum, targetSum, allPaths, currentPathValues);
+            }
+
+            if (currentSum == targetSum)
+            {
+                allPaths.Add(new List<T>(currentPathValues));
+            }
+
+            currentSum -= Convert.ToInt32(node.Key);
+            currentPathValues.RemoveAt(currentPathValues.Count - 1);
+        }
+
+        private int SubTreesWithGivenSumDFS(
+            Tree<T> node,
+            int targetSum,
+            List<Tree<T>> allTrees)
+        {
+            var currentSum = Convert.ToInt32(node.Key);
+            foreach (var child in node.Children)
+            {
+                currentSum += this.SubTreesWithGivenSumDFS(child, targetSum, allTrees);
+            }
+
+            if (currentSum == targetSum)
+            {
+                allTrees.Add(node);
+            }
+
+            return currentSum;
         }
 
         public List<Tree<T>> SubTreesWithGivenSum(int sum)
         {
-            throw new NotImplementedException();
+            var result = new List<Tree<T>>();
+
+            this.SubTreesWithGivenSumDFS(this, sum, result);
+
+            return result;
+
+            //var leafNodes = this.GetLeafNodes();
+            //var result = new List<Tree<T>>();
+
+            //foreach (var leaf in leafNodes)
+            //{
+            //    if (leaf.Children.Count == 0)
+            //    {
+            //        var leafNode = leaf;
+            //        var currentSum = 0;
+
+            //        if (Convert.ToInt32(leafNode.Key) == sum)
+            //        {
+            //            result.Add(leafNode);
+            //            continue;
+            //        }
+
+            //        while (leafNode.Parent != null)
+            //        {
+            //            leafNode = leafNode.Parent;
+            //            currentSum += Convert.ToInt32(leafNode.Key);
+            //            foreach (var childTree in leafNode.Children)
+            //            {
+            //                currentSum += Convert.ToInt32(childTree.Key);
+            //            }
+
+            //            if (currentSum == sum)
+            //            {
+            //                if (!result.Contains(leafNode))
+            //                {
+            //                    result.Add(leafNode);
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
+
+            //return result;
+
+            //var result = new List<Tree<T>>();
+
+            //var queue = new Queue<Tree<T>>();
+            //queue.Enqueue(this);
+
+            //while (queue.Count != 0)
+            //{
+            //    var currentSum = 0;
+            //    var node = queue.Dequeue();
+
+            //    foreach (var child in node.Children)
+            //    {
+            //        if (child.Children.Count == 0)
+            //        {
+            //            var childNode = child;
+
+            //            if (Convert.ToInt32(childNode.Key) == sum)
+            //            {
+            //                result.Add(childNode);
+            //                continue;
+            //            }
+
+            //            while (childNode.Parent != null)
+            //            {
+            //                childNode = childNode.Parent;
+            //                currentSum += Convert.ToInt32(childNode.Key);
+            //                foreach (var childTree in childNode.Children)
+            //                {
+            //                    currentSum += Convert.ToInt32(childTree.Key);
+            //                }
+
+            //                if (currentSum == sum)
+            //                {
+            //                    if (!result.Contains(childNode))
+            //                    {
+            //                        result.Add(childNode);
+            //                    }
+            //                }
+
+            //                currentSum = 0;
+            //            }
+            //        }
+
+            //        queue.Enqueue(child);
+            //    }
+            //}
+
+            //return result;
         }
     }
 }
