4a7462167ff4654f81041dc313d2cc06cc12162f
Georgi Tsekov <georgi_tsekov@abv.bg>
2022-06-07 17:41:54 +0300

    Implement MultiplicationTable of prime numbers. The console app needs a little refactoring and some tests for tomorrow.

---
+++ Algorithms/MultiplicationTable/MultiplicationTable/MultiplicationTable/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace MultiplicationTable
+{
+    public static class Constants
+    {
+        public const int FIRST_NUMBER = 1;
+        public const int SECOND_NUMBER = 2;
+        public const int INDENT = 10;
+    }
+}
