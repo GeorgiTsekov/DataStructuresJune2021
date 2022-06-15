a7b2a2d9ff61b2b3b66a6b94294c184f4d92e1a0
Georgi Tsekov <georgi_tsekov@abv.bg>
2022-06-15 12:09:01 +0300

    Some algorithmic exercises: NestedLoopsToRecursion

---
+++ Algorithms/RecursionAndCombinatorialProblems/NestedLoopsToRecursion/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace NestedLoopsToRecursion
+{
+    class Program
+    {
+        private static int[] arr;
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            arr = new int[n];
+            NestedLoop(0);
+        }
+
+        private static void NestedLoop(int index)
+        {
+            if (index >= arr.Length)
+            {
+                Console.WriteLine(string.Join(" ", arr));
+                return;
+            }
+
+            for (int i = 1; i <= arr.Length; i++)
+            {
+                arr[index] = i;
+                NestedLoop(index + 1);
+            }
+        }
+    }
+}
