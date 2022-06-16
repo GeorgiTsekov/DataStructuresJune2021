c46a227453222e876b8c58b37bfa71e47873efd5
Georgi Tsekov <georgi_tsekov@abv.bg>
2022-06-16 15:08:31 +0300

    solve some algorithmic tasks

---
+++ Algorithms/RecursionAndCombinatorialProblems/CinemaPlacesForFriends/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CinemaPlacesForFriends
+{
+    class Program
+    {
+        private static List<string> friends;
+        private static string[] staticFriends;
+        private static bool[] locked;
+
+        static void Main()
+        {
+            friends = Console.ReadLine().Split(", ").ToList();
+            staticFriends = new string[friends.Count];
+            locked = new bool[friends.Count];
+
+            string input = Console.ReadLine();
+            while (input != "generate")
+            {
+                var splitedInput = input.Split(" - ");
+                string name = splitedInput[0];
+                int position = int.Parse(splitedInput[1]) - 1;
+
+                if (staticFriends.Contains(name))
+                {
+                    Console.WriteLine($"{name} doesn't exist in the collection of friends!");
+                    return;
+                }
+
+                if (locked[position])
+                {
+                    Console.WriteLine($"2 friends want equal places!");
+                    return;
+                }
+
+                staticFriends[position] = name;
+                locked[position] = true;
+                friends.Remove(name);
+
+                input = Console.ReadLine();
+            }
+
+            Permute(0);
+        }
+
+        private static void Permute(int index)
+        {
+            if (index >= friends.Count)
+            {
+                PrintFriends();
+                return;
+            }
+
+            Permute(index + 1);
+
+            for (int i = index + 1; i < friends.Count; i++)
+            {
+                Swap(index, i);
+                Permute(index + 1);
+                Swap(index, i); 
+            }
+        }
+
+        private static void PrintFriends()
+        {
+            int indexOfFriends = 0;
+            var sb = new StringBuilder();
+            for (int i = 0; i < staticFriends.Length; i++)
+            {
+                if (locked[i])
+                {
+                    sb.Append(staticFriends[i] + " ");
+                }
+                else
+                {
+                    sb.Append(friends[indexOfFriends++] + " ");
+                }
+            }
+
+            Console.WriteLine(String.Join(" ", sb.ToString().TrimEnd()));
+        }
+
+        private static void Swap(int first, int second)
+        {
+            var temp = friends[first];
+            friends[first] = friends[second];
+            friends[second] = temp;
+        }
+    }
+}
---
+++ Algorithms/RecursionAndCombinatorialProblems/SchoolTeams/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SchoolTeams
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var girls = Console.ReadLine().Split(", ");
+            var girlsComb = new string[3];
+            var girlsCombs = new List<string[]>();
+            var boys = Console.ReadLine().Split(", ");
+            var boysComb = new string[2];
+            var boysCombs = new List<string[]>();
+
+            GenCombinations(0, 0, girls, girlsComb, girlsCombs);
+            GenCombinations(0, 0, boys, boysComb, boysCombs);
+            PrintAllCombinations(girlsCombs, boysCombs);
+        }
+
+        private static void PrintAllCombinations(List<string[]> girlsCombs, List<string[]> boysCombs)
+        {
+            foreach (var girlsComb in girlsCombs)
+            {
+                foreach (var boysComb in boysCombs)
+                {
+                    Console.WriteLine($"{String.Join(", ", girlsComb)}, {string.Join(", ", boysComb)}");
+                }
+            }
+        }
+
+        private static void GenCombinations(int index, int firstElement, string[] elements, string[] combination, List<string[]> combinationsList)
+        {
+            if (index >= combination.Length)
+            {
+                combinationsList.Add(combination.ToArray());
+                return;
+            }
+
+            for (int i = firstElement; i < elements.Length; i++)
+            {
+                combination[index] = elements[i];
+                GenCombinations(index + 1, i + 1, elements, combination, combinationsList);
+            }
+        }
+    }
+}
