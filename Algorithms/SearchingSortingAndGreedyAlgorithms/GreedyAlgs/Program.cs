7f6d06cfbaf7d7e5318e161a52680d0295044c96
Georgi Tsekov <georgi_tsekov@abv.bg>
2022-06-20 18:17:17 +0300

    Some tasks with Greedy algorithms

---
+++ Algorithms/SearchingSortingAndGreedyAlgorithms/GreedyAlgs/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SumOfCoins
+{
+    class Program
+    {
+        static void Main()
+        {
+            var coins = new Queue<int>(Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .OrderByDescending(x => x));
+
+            int target = int.Parse(Console.ReadLine());
+            var result = new Dictionary<int, int>();
+            
+            while (target > 0 && coins.Count > 0)
+            {
+                int coin = coins.Dequeue();
+                int countOfCoin = target / coin;
+
+                if (countOfCoin > 0)
+                {
+                    target %= coin;
+                    result.Add(coin, countOfCoin);
+                }       
+            }
+
+            if (target == 0)
+            {
+                Console.WriteLine(result.Values.Sum());
+                foreach (var kvp in result)
+                {
+                    Console.WriteLine($"{kvp.Value} X {kvp.Key} = {kvp.Key * kvp.Value}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Can't do it");
+            }
+        }
+    }
+}
---
+++ Algorithms/SearchingSortingAndGreedyAlgorithms/SetCover/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SetCover
+{
+    // input    
+    // 1 3 5 7 9 11 20 30 40
+    // 6
+    // 20
+    // 1 5 20 30
+    // 3 7 20 30 40
+    // 9 30
+    // 11 20 30 40
+    // 3 7 40
+    class Program
+    {
+        static void Main()
+        {
+            var universe = Console.ReadLine()
+                .Split(" ")
+                .Select(int.Parse)
+                .ToHashSet();
+
+            var n = int.Parse(Console.ReadLine());
+
+            var sets = new List<int[]>();
+
+            for (int i = 0; i < n; i++)
+            {
+                var set = Console.ReadLine()
+                    .Split()
+                    .Select(int.Parse)
+                    .ToArray();
+
+                sets.Add(set);
+            }
+
+            var selectedSets = new List<int[]>();
+            while (universe.Count > 0)
+            {
+                var set = sets
+                    .OrderByDescending(x => x.Count(e => universe.Contains(e)))
+                    .FirstOrDefault();
+
+                selectedSets.Add(set);
+
+                sets.Remove(set);
+
+                foreach (var element in set)
+                {
+                    universe.Remove(element);
+                }
+            }
+
+            Console.WriteLine(selectedSets.Count);
+
+            foreach (var set in selectedSets)
+            {
+                Console.WriteLine(string.Join(" ", set));
+            }
+        }
+    }
+}
