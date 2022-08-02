e52eba6c0370e10b026c8644ab139fa3db7d9da8
Georgi Tsekov <georgi_tsekov@abv.bg>
2022-08-02 17:46:45 +0300

    Fibonachi and LonguestIncreasingSubsequence solved with dynamic programming

---
+++ Algorithms/SearchingSortingAndGreedyAlgorithms/Fibonachi/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Fibonachi
+{
+    class Program
+    {
+        private static long[] numbers;
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            numbers = new long[n + 1];
+            Console.WriteLine(Fibonachi(n));
+        }
+
+        private static long Fibonachi(int n)
+        {
+            if (numbers[n] != 0)
+            {
+                return numbers[n];
+            }
+
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+
+            var result =  Fibonachi(n - 1) + Fibonachi(n - 2);
+
+            numbers[n] = result;
+
+            return result;
+        }
+    }
+}
---
+++ Algorithms/SearchingSortingAndGreedyAlgorithms/LonguestIncreasingSubsequence/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LonguestIncreasingSubsequence
+{
+    class Program
+    {
+        private static Dictionary<int, List<int>> sequences;
+        static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            sequences = new Dictionary<int, List<int>>();
+
+            var longuestSequence = LonguestIncreasingSubsequence(numbers);
+            Console.WriteLine(string.Join(" ", longuestSequence));
+        }
+
+        private static List<int> LonguestIncreasingSubsequence(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                int number = numbers[i];
+                foreach (var sequence in sequences.Values)
+                {
+                    if (sequence.Last() <= number)
+                    {
+                        sequence.Add(number);
+                    }
+                }
+
+                sequences.Add(i, new List<int>());
+                sequences[i].Add(number);
+            }
+
+            var longuestSequence = sequences.Values.OrderByDescending(x => x.Count).FirstOrDefault();
+            return longuestSequence;
+        }
+    }
+}
